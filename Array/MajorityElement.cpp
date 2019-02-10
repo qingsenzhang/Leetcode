dbf3eb3a04379dfe06b2d8b314f88be413d9f5c9
Zekun Zhang <qingsen@Zekuns-MacBook-Pro.local>
2019-02-10 01:03:00 -0500

    improved majorityelement

--- Array/MajorityElement.cpp
+++ Array/MajorityElement.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        /**
         unordered_map<int, int> hash;
         for (int i = 0; i < nums.size(); i++) {
             if (hash.find(nums[i]) != hash.end()) {
@@ -15,5 +16,22 @@ public:
         }
         //note the case where  nums has length one
         return nums[0];
+        **/
+
+        //The key of this problem is to use a smart and intuitive moore voting algorithm
+        int major = nums[0], count = 1;
+        //note that since we initialized major with nums[0], we start i with 1
+        for (int i = 1; i < nums.size(); i++) {
+            if (count == 0) {
+                major = nums[i];
+                count++;
+            } else if (nums[i] == major) {
+                count++;
+            } else {
+                count--;
+            }
+        }
+
+        return major;
     }
 };
