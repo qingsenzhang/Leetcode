288c1e67778c9ff3b518a434e69306442a64ca73
Zekun Zhang <qingsen@Zekuns-MacBook-Pro.local>
2019-02-09 20:25:02 -0500

    improved twoSum

--- Array/twoSum.cpp
+++ Array/twoSum.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        map<int, int> numbers;
+        //here remember to use unordered_map rather than map, the data is faster for retrieval 
+        //map has O(log(n)) for search/insertion/deletion, while unordered_map has O(1) (best) and O(n) (worst)
+        unordered_map <int, int> numbers;
         vector<int> vect;
+
         for (int i = 0; i < nums.size(); i++) {
             if (numbers.find(target - nums[i]) != numbers.end()) {
                 vect = {numbers[target - nums[i]], i};
@@ -13,5 +16,5 @@ public:
 
         return vect;
     }
-}
+};
 
