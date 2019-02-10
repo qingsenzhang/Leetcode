19b6a34c3be046bde88c861c429411c5eb0e8566
Zekun Zhang <qingsen@Zekuns-MacBook-Pro.local>
2019-02-09 20:12:17 -0500

    first attemp twosum

--- Array/twoSum.cpp
+++ Array/twoSum.cpp
@@ -1,6 +1,20 @@
-class Solution {
+ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        
+        map<int, int> numbers;
+        vector<int> vect;
+        for (int i = 0; i < nums.size(); i++) {
+            if (numbers.find(target - nums[i]) != numbers.end()) {
+                vect = {numbers[target - nums[i]], i};
+                return vect;
+            }
+            numbers[nums[i]] = i;
+        }
+
+        return vect;
     }
-};
+};~
+ ~
+ ~
+ ~
+ ~
