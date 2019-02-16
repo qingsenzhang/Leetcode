4c92d3aa9ed253fd542666ce8a3f020e1ca3ff61
Zekun Zhang <qingsen@Zekuns-MacBook-Pro.local>
2019-02-16 16:28:23 -0500

    improved productofarrayexceptself

--- Array/ProductOfArrayExceptSelf.cpp
+++ Array/ProductOfArrayExceptSelf.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
+	// The key of this problem is to break the multipying process into two distinct 
+	//passes: one pass multiplying all the numbers from left and the other 
+	//multipying from right
     vector<int> productExceptSelf(vector<int>& nums) {
     	if (nums.size() == 0) return vector<int>();
+    	/**
         vector<int> ret(nums.size());
 
         ret[0] = 1;
@@ -14,6 +18,15 @@ public:
         	ret[i] *= right;
         	right *= nums[i];
         }
+        **/
+    	vector<int> ret(nums.size(), 1);
+    	int left = 1, right = 1, n = nums.size();
+    	for (int i = 0; i < n; i++) {
+    		ret[i] *= left;
+    		left *= nums[i];
+    		ret[n - i - 1] *= right;
+    		right *= nums[n - i - 1];
+    	}
 
         return ret;
     }
