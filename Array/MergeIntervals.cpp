08104b64208752bb600e475404d2e508483d19cd
Zekun Zhang <qingsen@Zekuns-MacBook-Pro.local>
2019-02-23 01:53:04 -0500

    improved version of mergeintervals

--- Array/MergeIntervals.cpp
+++ Array/MergeIntervals.cpp
@@ -9,13 +9,14 @@
  */
 class Solution {
 public:
-	static bool compareInterval(Interval v1, Interval v2) {
-		return (v1.start < v2.start);
-	}
 
+	//Improved version of merge interval. This approach does not need to 
+	//sort the vector before processing it but its time complexity is 
+	//still the same as before
     vector<Interval> merge(vector<Interval>& intervals) {
         if (intervals.size() == 0) return vector<Interval>();
 
+        /**
         sort(intervals.begin(), intervals.end(), compareInterval);
 
         int curr = 0;
@@ -29,5 +30,21 @@ public:
         }
 
         return intervals;
+        **/
+        map<int, int> intervs;
+        for (auto &i: intervals) {
+        	int start = i.start, end = i.end;
+        	auto itr = intervs.lower_bound(i.start);
+        	while (itr != intervs.end() && itr->second <= i.end) {
+        		start = min(start, itr->second);
+        		end = max(end, itr->first);
+        		itr = intervs.erase(itr);
+        	}
+        	intervs[end] = start;
+        }
+
+        vector<Interval> ret;
+        for (auto itr = intervs.begin(); itr != intervs.end(); itr++)  {ret.push_back(Interval(itr->second, itr->first));}
+        return ret;
     }
 };
